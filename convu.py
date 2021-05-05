8df73fe59f868644c14d13354a0e9ff620991af7
Naiztu <A01707404@itesm.mx>
2021-05-05 17:50:00 -0500

    Implementación de metodo convolution

--- convu.py
+++ convu.py
@@ -14,9 +14,16 @@ def multi_mat(matriz, kernel):
         for col in range(m_col):
             sum += matriz[row, col] * kernel[row,col]
     return sum 
-def main():
-    mat = np.array(([11, 12, 13], [21, 22, 23], [31, 32, 33]))
-    ker = np.array(([1, 0, 1], [1, 1, 1], [0, 1, 0]))
-    print(multi_mat(mat,ker))
-main()
+
+def convolution(img, kernel):
+    img_row, img_col = img.shape
+    k_row, k_col = kernel.shape
+
+    mat_aux = np.zeros((img_row-2, img_col-2))
+
+    for row in range (mat_aux.shape[0]):
+        for col in range (mat_aux.shape[1]):
+            mat_aux[row, col] = multi_mat(img[row: row + k_row, col: col + k_col], kernel)
+    return mat_aux
+
 
