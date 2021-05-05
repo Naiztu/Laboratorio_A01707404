f97aab6cd5c02fe724a248ba91f0f1292e905f30
Naiztu <A01707404@itesm.mx>
2021-05-05 17:52:40 -0500

    Caso de prueba

--- convu.py
+++ convu.py
@@ -26,4 +26,9 @@ def convolution(img, kernel):
             mat_aux[row, col] = multi_mat(img[row: row + k_row, col: col + k_col], kernel)
     return mat_aux
 
+def main():
+    mat = np.array(([1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12], [0, 0, 1, 16, 17, 18], [0, 1, 0, 7, 23, 24], [1, 7, 6, 5, 4, 3]))
+    ker = np.array(([1, 1, 1], [0, 0, 0], [2, 10, 3]))
+    print(convolution(mat,ker))
+main()
 
